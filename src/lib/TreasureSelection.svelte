7071ba5637f18ab550c9cbbcd64faed48793e21d
c8e4 <c8e4@proton.me>
2023-10-15 12:06:29 +0200

    add nft metadata

--- src/lib/TreasureSelection.svelte
+++ src/lib/TreasureSelection.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
-	import { ALL_TREASURES } from '$lib/store/store.js';
+	import { ALL_TREASURES, selected_treasure } from '$lib/store/store.js';
+	function clickPreviousChest(){
+		if($selected_treasure.id > 0){
+			selected_treasure.set(ALL_TREASURES[$selected_treasure.id-1])
+		}
+	}
+	function clickNextChest(){
+		if($selected_treasure.id < ALL_TREASURES.length-1){
+			selected_treasure.set(ALL_TREASURES[$selected_treasure.id+1])
+		}
+	}
 </script>
 
-{#each ALL_TREASURES as chest}
-	<img src="{chest.img}" alt="" />
-{/each}
+<!-- {#each ALL_TREASURES as chest} -->
+<div class="flex items-center gap-4">
+	<button disabled={$selected_treasure.id == 0} on:click={clickPreviousChest} style="width:100px;height:100px;">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+			><path fill={$selected_treasure.id == 0?'#CCC3':'#CCCD'} d="M3 12l18-12v24z" /></svg
+		>
+	</button>
+	<img src={$selected_treasure.img} alt="" />
+	<button disabled={$selected_treasure.id == ALL_TREASURES.length-1} on:click={clickNextChest} style="width:100px;height:100px;">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+			><path fill={$selected_treasure.id == ALL_TREASURES.length-1?'#CCC3':'#CCCD'} d="M21 12l-18 12v-24z" /></svg
+		>
+	</button>
+</div>
+<!-- {/each} -->
--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -44,14 +44,41 @@ function removeConfirmedBoxes(confirmedBoxes){
 
 export const ALL_TREASURES = [
     {
+        id: 0,
+        price: 10,
         img: 'chest/10erg.png',
+        cid: 'bafybeihui4r6ylwy2u7crtvhllwy4rnorop3icpgvks5edkyb47f3guziq',
+        sha256: 'e8f26588981063e0bd9fd3f4b05b2f42aac53a0c17fd1e265b3f1eef27466b44',
+        name: '$200.00 Hodlbox'
     },{
+        id: 1,
+        price: 100,
         img: 'chest/100erg.png',
+        cid: 'bafybeideasgh2la3p4c3pqvwjzcjevyxr6j7wawtqqfa6rc25a6ntogk5i',
+        sha256: 'df338cc3cce006d349dd3f4c4ca97bc497733906e20afb178f982e343548989d',
+        name: '$2,000.00 Hodlbox'
     },{
+        id: 2,
+        price: 1000,
         img: 'chest/1000erg.png',
+        cid: 'bafybeigkm4chflhvptckwprjmhvps5kcpuzevgjcthak5q435dv4os2x54',
+        sha256: '5704bd829777b113026534fd24c8f0e89185090f6cd36b087532173ca9d26e5e',
+        name: '$20,000.00 Hodlbox'
     },{
+        id: 3,
+        price: 10000,
         img: 'chest/10000erg.png',
+        cid: 'bafybeicihtoti7v4ul5l4aisp2ccb7feoatoqyjtxw4clw6jk676gfkuhm',
+        sha256: 'ed40bd94ca81b53e4fe930be0e0d81e1f3fabf336ba5c26311ec55e633542d59',
+        name: '$200,000.00 Hodlbox'
     },{
+        id: 4,
+        price: 1.27,
         img: 'chest/1.27erg.png',
+        cid: 'bafkreia4wvsv6j2ka6s6sso5vkmemmkx4risp5o5i3wk3cbbtkplzcmjpe',
+        sha256: '1cb5655f274a07a5e949ddaa98463157e45127f5dd46ecad88219a9ebc898979',
+        name: '$25.40 Hodlbox'
     },
 ]
+
+export const selected_treasure = writable(ALL_TREASURES[0]);
