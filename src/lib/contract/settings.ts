a3d8e4a0f3a712bece840c621d7b5ccb255f62a1
Savonarola <x@x.com>
2023-10-15 15:09:13 +0400

    dev_ui_pk

--- src/lib/contract/settings.ts
+++ src/lib/contract/settings.ts
@@ -2,5 +2,5 @@ export const DEV_CONTRACT_PK = '9ffXZz5AovJvapPo63TGwdNaRPMUiHo2UkqGavmDGzrUERY9
 export const _contractDevPK   = DEV_CONTRACT_PK
 export const contractBase58PK = DEV_CONTRACT_PK
 
-export const DEV_UI_PK = ''
+export const DEV_UI_PK = '9fxHfNqjT4ELqxbBU3RyvmTretGh88btXFHPBHvXC5RaZoMLKqt'
 export const uiBase58PK       = DEV_UI_PK
