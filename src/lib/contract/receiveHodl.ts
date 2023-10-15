a4f89f01041c2a76f21810b0356550e52a91196b
c8e4 <c8e4@proton.me>
2023-10-15 09:36:57 +0200

    refactor

--- src/lib/contract/receiveHodl.ts
+++ src/lib/contract/receiveHodl.ts
@@ -2,13 +2,13 @@ import { ErgoAddress, OutputBuilder, RECOMMENDED_MIN_FEE_VALUE, SAFE_MIN_BOX_VAL
 import { getOracleBox } from "./getOracleBox.js";
 import { DEV_CONTRACT_PK, DEV_UI_PK } from "./settings.js";
 
-export async function receiveHodlBoxTx(buyBox: object, holderBase58PK: string, utxos: Array<any>, height: number, devFeeBase58PK: string = DEV_CONTRACT_PK,  uiFeeBase58PK: string = DEV_UI_PK): any {
+export async function receiveHodlBoxTx(hodlBox: object, holderBase58PK: string, utxos: Array<any>, height: number, devFeeBase58PK: string = DEV_CONTRACT_PK,  uiFeeBase58PK: string = DEV_UI_PK): any {
 
     const myAddr = ErgoAddress.fromBase58(holderBase58PK)
 
     const oracleBox = await getOracleBox()
 
-    const value = BigInt(buyBox.value)
+    const value = BigInt(hodlBox.value)
     let devFeeAmount = value / 200n
     let uiFeeAmount = value / 200n
     let userAmount = value - devFeeAmount - uiFeeAmount
@@ -16,7 +16,7 @@ export async function receiveHodlBoxTx(buyBox: object, holderBase58PK: string, u
     const hodlerBox = new OutputBuilder(
         userAmount,
         myAddr
-    ).addTokens(buyBox.assets)
+    ).addTokens(hodlBox.assets)
 
     const devFeeBox = new OutputBuilder(
         SAFE_MIN_BOX_VALUE > devFeeAmount ? SAFE_MIN_BOX_VALUE : devFeeAmount,
@@ -30,8 +30,8 @@ export async function receiveHodlBoxTx(buyBox: object, holderBase58PK: string, u
     const outputBoxes = [hodlerBox, devFeeAmount > 0n ? devFeeBox : undefined, uiFeeAmount > 0n ? uiFeeBox : undefined].filter(x => x)
 
     const unsignedMintTransaction = new TransactionBuilder(height)
-        .configureSelector((selector) => selector.ensureInclusion(buyBox.boxId))
-        .from([buyBox, ...utxos])
+        .configureSelector((selector) => selector.ensureInclusion(hodlBox.boxId))
+        .from([hodlBox, ...utxos])
         .to(outputBoxes)
         .sendChangeTo(myAddr)
         .payFee(RECOMMENDED_MIN_FEE_VALUE)
