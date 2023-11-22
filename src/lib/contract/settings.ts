3d48bafa0332bb445955e86f448f5b0093abb66a
c8e4 <c8e4@proton.me>
2023-11-22 13:03:14 +0100

    update walrus address

--- src/lib/contract/settings.ts
+++ src/lib/contract/settings.ts
@@ -4,7 +4,9 @@ export const contractBase58PK = DEV_CONTRACT_PK
 
 export const HODLBOX_DEV_UI_PK = '9fxHfNqjT4ELqxbBU3RyvmTretGh88btXFHPBHvXC5RaZoMLKqt'
 export const BITMASKS_DEV_UI_PK = '9gdLSKyzeyB3qQ1LWidALiyyfvQwFCZtcozXqjQ9hRkfGwPFCbR'
-export const WALRUS_DEV_UI_PK = '9fjaoqKoTtuFUZMmkeA7quf2WotQwsq8EUYK5ZfCvZjCqWaW5B8'
+//export const WALRUS_DEV_UI_PK = '9fjaoqKoTtuFUZMmkeA7quf2WotQwsq8EUYK5ZfCvZjCqWaW5B8' <= 1 test minted
+export const WALRUS_DEV_UI_PK = '9hGceS9rTZDDmsnioJGWykfUAzk8ZbCuuadNLvoeG7hN9bM3kNP'
+
 
 export const HODLERG3_TOKEN_ID='cbd75cfe1a4f37f9a22eaee516300e36ea82017073036f07a09c1d2e10277cda' // HOLDERG3
 //export const HODLERG3_TOKEN_ID = 'f57dfb23f768a2f85cd88bc453e4479af870410bf132e7d182718da50d9aa34b' // HOLDERG3 TEST 
